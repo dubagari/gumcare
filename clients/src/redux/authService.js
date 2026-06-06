b4fb62d8864769a8b38ed4a48a53edaec565dbb9
Abubakar Abdullahi Dubagari <superango93@gmail.com>
2026-06-06 19:15:52 +0100

    client login fix

--- clients/src/redux/authService.js
+++ clients/src/redux/authService.js
@@ -1,79 +1,27 @@
-const API_URL = "/api/auth";
-
-// Register user
-// const signupuser = async (userData) => {
-//   const response = await fetch(`${API_URL}/signupuser`, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(userData),
-//   });
-
-//   const data = await response.json();
-
-//   if (!response.ok) {
-//     throw new Error(data.message || 'Registration failed');
-//   }
-
-//   if (data) {
-//     localStorage.setItem('user', JSON.stringify(data));
-//   }
-
-//   return data;
-// };
-
-// // Login user
-// const login = async (userData) => {
-//   const response = await fetch(`${API_URL}/loginuser`, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(userData),
-//   });
-
-//   const data = await response.json();
-
-//   if (!response.ok) {
-//     throw new Error(data.message || 'Login failed');
-//   }
-
-//   if (data) {
-//     localStorage.setItem('user', JSON.stringify(data));
-//   }
-
-//   return data;
-// };
-
-// authService.js
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 const signupuser = async (userData) => {
-  const res = await fetch(`${API_URL}/signupuser`, {
+  const res = await fetch(`${API_URL}/auth/signupuser`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(userData),
   });
 
   const data = await res.json();
-
   if (!res.ok) throw new Error(data.message);
-
-  return data; // MUST return full object
+  return data;
 };
 
 const login = async (userData) => {
-  const res = await fetch(`${API_URL}/loginuser`, {
+  const res = await fetch(`${API_URL}/api/auth/loginuser`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(userData),
   });
 
   const data = await res.json();
-
   if (!res.ok) throw new Error(data.message);
-
-  return data; // MUST return full object
+  return data;
 };
 // Logout user
 const logout = () => {
